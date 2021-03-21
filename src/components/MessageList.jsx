24290da16124d18342ca89e79fad83d253968838
VASh <shokarev.vl@gmail.com>
2021-03-21 11:08:11 +0300

    fixed bug & added bot

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import { Message } from './Message'
 
 export const MessageList = (props) => {
-  return props.items.map((item, index) => <Message text={item} author={props.author} key={index}/>)
+  return props.items.map((item, index) => (<Message text={item.text} author={item.author} key={index} />))
 }
--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -4,22 +4,53 @@ import { MessageList } from './MessageList'
 
 export class Messenger extends React.Component{
   state = {
-    author: 'author',
-    message: ['Hello', 'Hi', 'Hallo', 'Hola']
+    messages: [{
+      author: 'author1',
+      text: 'Hello'
+    },
+    {
+      author: 'author2',
+      text: 'Hi'
+    }]
   }
+
+  interval = null
 
   handleMessageSend = (message) => {
-    this.setState({
-      author: message.author,
-      message: this.state.message.concat([message.text])
-    })
+    this.setState({messages: this.state.messages.concat(message)})
+    clearInterval(this.interval);
   }
 
+  randomMessage(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+componentDidUpdate() {
+    clearInterval(this.interval)
+
+    const lastAuthor = this.state.messages[this.state.messages.length - 1].author
+
+    const botAnswer = [`Привет, ${lastAuthor}, чем я могу тебе помочь?`, `${lastAuthor}, спроси что-нибудь проще.`, `Очень интересная история, ${lastAuthor}`, `Не согласен с тобой, ${lastAuthor}.`, `Привет, ${lastAuthor}, приятно познакомиться!`, `${lastAuthor}, повтори, пожалуйста.`, `${lastAuthor}, полностью согласен!`, `${lastAuthor}, как дела?`, `${lastAuthor}, погода и правда сегодня хорошая.`, `${lastAuthor}, пока!`,]
+
+    const index = this.randomMessage(1, 10)
+
+    const answer = {
+        author: "Bot",
+        text: botAnswer[index - 1],
+    }
+
+    this.interval = setInterval(() => {
+        this.handleMessageSend(answer)
+    }, 1000)
+
+}
+
   render(){
-    const {author, message} = this.state
+    const { messages } = this.state
     return (
       <>
-        <MessageList items={message} author={author}/>
+        <MessageList items={messages} author={messages}/>
         <MessageForm onSend={this.handleMessageSend}/>
       </>
     )
