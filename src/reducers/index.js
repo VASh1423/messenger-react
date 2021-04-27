63fd1c22262d28cca0488d3d01756658cd712819
VASh <shokarev.vl@gmail.com>
2021-04-27 22:37:05 +0300

    fixed bug

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,9 @@ import { connectRouter } from 'connected-react-router'
 import { chatReducer } from './chats'
 import { profileReducer } from './profile'
 
-export const createRootReducer = (history) => {
+export const createRootReducer = (history) => 
   combineReducers({
     router: connectRouter(history),
     chats: chatReducer,
     profile: profileReducer,
   })
-}
