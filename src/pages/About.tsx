ead37cfe3428c7226dad6630dc76b41b9447e379
VASh <shokarev.vl@gmail.com>
2021-04-27 21:13:03 +0300

    prettier test

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import './pages.css'
 
 export const About: React.FC = () => (
-  <div className="pages_container">
-  <div>
+  <div className="pages_container"><div>
       <h1>О нас</h1>
 <p>Страница о нас</p>
-    </div>
+  </div>
   </div>
 )
